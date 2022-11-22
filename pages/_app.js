49d7eef82aadeab56e1eb45bbcd4bb14cca81ccb
conor vanoystaeyen <conor.vanoystaeyen@hotmail.com>
2022-11-22 21:27:12 +0100

    banner comp & cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer>
+        <p>© 2022 Conor-v</p>
+      </footer>
+    </>    
+  )
 }
 
 export default MyApp
