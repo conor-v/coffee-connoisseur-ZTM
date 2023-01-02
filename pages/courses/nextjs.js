9f84a708489660158aa1fbadda5bd65321b968e1
conor vanoystaeyen <conor.vanoystaeyen@hotmail.com>
2023-01-02 20:33:24 +0100

    hero img toevoegen

--- pages/courses/nextjs.js
+++
@@ -1,9 +0,0 @@
-const Nextjs = () => {
-  return (
-    <div>
-      <p>Welcome to Next.js with Conor-v</p>
-    </div>
-  )
-}
-
-export default Nextjs
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image';
 import Banner from '../components/banner'
 import styles from '../styles/Home.module.css'
 
@@ -18,6 +19,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <Banner buttonText="View stores nearby" handleOnclick={handleOnBannerBtnClick}/>
+        <div className={styles.heroImage}>
+          <Image src="/static/hero-image.png" width={700} height={400}/>
+        </div>
       </main>
 
 
