7e4008dbb806389402604e092788ebb19785693a
conor vanoystaeyen <conor.vanoystaeyen@hotmail.com>
2023-01-14 16:59:41 +0100

    staticpaths implementeren

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,7 +1,42 @@
-const CoffeeStore = () => {
-  return (
-    <p>coffee-store id</p>
-  )
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export async function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id.toString() === params.id; //dynamic id
+      }),
+    },
+  };
 }
 
-export default CoffeeStore
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }, { params: { id: "300" } }],
+    fallback: false
+  };
+}
+
+const CoffeeStore = (props) => {
+  const router = useRouter();
+  console.log("router", router);
+  return (
+    <div>
+      Coffee Store Page {router.query.id}
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+      <Link href="/coffee-store/dynamic">
+        <a>Go to page dynamic</a>
+      </Link>
+    
+      <p>{props.coffeeStore.address}</p>
+      <p>{props.coffeeStore.name}</p>
+    </div>
+  );
+};
+export default CoffeeStore;
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import Image from 'next/image';
 import Banner from '../components/banner'
 import Card from '../components/card';
 import styles from '../styles/Home.module.css'
-import coffeeStores from '../data/coffee-stores.json'
+import coffeeStoresData from '../data/coffee-stores.json'
 
 export async function getStaticProps(context) {
   return {
     props: {
-      coffeeStores,
+      coffeeStores: coffeeStoresData,
     },
   };
 }
@@ -31,17 +31,22 @@ export default function Home(props) {
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400}/>
         </div>
-        <div className={styles.cardLayout}>
-          {props.coffeeStores.map((coffeeStore) => (
-            <Card 
-              key={coffeeStore.id}
-              name={coffeeStore.name}
-              imgUrl={coffeeStore.imgUrl}
-              href={`/coffee-store/${coffeeStore.id}`}
-              className={styles.card}
-            />
-          ))}
-        </div>
+        {props.coffeeStores.length > 0 &&
+          <>
+            <h2 className={styles.heading2}>Torronto stores</h2>
+            <div className={styles.cardLayout}>
+              {props.coffeeStores.map((coffeeStore) => (
+                <Card 
+                  key={coffeeStore.id}
+                  name={coffeeStore.name}
+                  imgUrl={coffeeStore.imgUrl}
+                  href={`/coffee-store/${coffeeStore.id}`}
+                  className={styles.card}
+                />
+              ))}
+            </div>
+          </>
+        }
       </main>
     </div>
   )
